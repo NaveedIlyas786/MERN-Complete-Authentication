3aec1854130d0c5ac079679b06f556303462d417
Naveed <naveedilyas321@gmail.com>
2024-05-02 15:56:24 +0500

    Changes for Correct deployment on Server

--- Server/app.js
+++ Server/app.js
@@ -8,12 +8,18 @@ const app = express();
 const port =8009;
 
 // //! Setting path and getting response from server at web page
-// app.get("/",(req,res)=>{
-//     res.status(201).json("Server Created");
-// })
+app.get("/",(req,res)=>{
+    res.json("Hello Sir! Server Deloyed");
+})
 
 app.use(express.json()); //? This is used to show the data in JSON form coming from frontend side
-app.use(cors());         //? Use here for recieving data from frontend side means from different port number
+app.use(cors(
+    {
+        origin:["http://mern-complete-authentication-server.vercel.app"],
+        methods:["POST","GET"],
+        credentials: true
+    }
+));         //? Use here for recieving data from frontend side means from different port number
 app.use(router);         //? use here for implementing the routing
 app.use(cookieparser());
 app.listen((port),()=>{
