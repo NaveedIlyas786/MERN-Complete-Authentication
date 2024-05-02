cd6b0311690137fbb87d8cd014e252a61ec8bd61
Naveed <naveedilyas321@gmail.com>
2024-05-02 15:59:07 +0500

    Changes for Correct deployment on Server1

--- Server/db/connection.js
+++ Server/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose =require("mongoose");
 
-// const DB = "mongodb://127.0.0.1:27017/MERNAuthentication?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5";
-const DB = "mongodb+srv://naveedilyas321:pingpong1122?@cluster0.kftvyxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DB = "mongodb://127.0.0.1:27017/MERNAuthentication?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.5";
+// const DB = "mongodb+srv://naveedilyas321:pingpong1122?@cluster0.kftvyxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(DB).then(()=>{
     console.log("Database Connected!");
